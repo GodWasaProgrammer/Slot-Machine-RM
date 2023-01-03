d78dc3ba8be99753f513c643147bd981017325f6
GodWasaProgrammer <bwnils@gmail.com>
2023-01-03 04:55:21 +0100

    Made an array, trying to fill it properly

--- Program.cs
+++ Program.cs
@@ -2,10 +2,24 @@
 {
     internal class Program
     {
+        const int MAXVALUE = 9;
+        const int MINVALUE = 0;
         static void Main()
         {
             Console.WriteLine("Welcome to our slot machine");
-            
+            int[,] slotArray = new int[3, 3] { { 0, 0, 0, }, { 0, 0, 0, }, { 0, 0, 0, } };
+
+            Random slotArrayRandom = new Random();
+            foreach (int i in slotArray)
+            {
+
+                slotArray[i, i] = slotArrayRandom.Next(MAXVALUE);
+            }
+
+            foreach (int i in slotArray)
+            {
+                Console.Write(i);
+            }
         }
     }
 }
