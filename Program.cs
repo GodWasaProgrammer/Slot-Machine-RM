a61aeffaf7d7000ab919f084de5fc787ba2afdb9
GodWasaProgrammer <bwnils@gmail.com>
2023-01-03 05:20:28 +0100

    we can now print a slot masheeeen

--- Program.cs
+++ Program.cs
@@ -6,20 +6,27 @@
         const int MINVALUE = 0;
         static void Main()
         {
+            Random slotArrayRandom = new Random();
             Console.WriteLine("Welcome to our slot machine");
             int[,] slotArray = new int[3, 3] { { 0, 0, 0, }, { 0, 0, 0, }, { 0, 0, 0, } };
 
-            Random slotArrayRandom = new Random();
-            foreach (int i in slotArray)
+            for (int i = 0; i < slotArray.GetLength(0); i++)
             {
-
-                slotArray[i, i] = slotArrayRandom.Next(MAXVALUE);
+                slotArray[i, 0] = slotArrayRandom.Next(MAXVALUE);
+                slotArray[i, 1] = slotArrayRandom.Next(MAXVALUE);
+                slotArray[i, 2] = slotArrayRandom.Next(MAXVALUE);
             }
 
-            foreach (int i in slotArray)
+            for (int i = 0; i < slotArray.GetLength(0); i++)
             {
-                Console.Write(i);
+                Console.Write($"{slotArray[i, 0]}");
+
+                Console.Write($"{slotArray[i, 1]}");
+
+                Console.WriteLine($"{slotArray[i, 2]}");
             }
+
+
         }
     }
 }
