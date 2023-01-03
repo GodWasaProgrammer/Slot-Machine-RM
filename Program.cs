b33d76e19fbef35484a77a2df6dd124e728624e5
GodWasaProgrammer <bwnils@gmail.com>
2023-01-03 01:40:39 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Slot_Machine_RM
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to our slot machine");
+            
+        }
+    }
+}
