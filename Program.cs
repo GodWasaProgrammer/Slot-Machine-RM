6adebe0f7c30ff6bfec8d731ea786697af2a8572
GodWasaProgrammer <bwnils@gmail.com>
2023-01-04 05:09:37 +0100

    beautified and corrected if statement wrongly claiming to check mid horizontal, it was checking bottom horizontal.

--- Program.cs
+++ Program.cs
@@ -3,13 +3,13 @@
     internal class Program
     {
         const int MAXVALUE = 9;
-        const int MINVALUE = 0;
+
         static void Main()
         {
             Random slotArrayRandom = new Random();
             Console.WriteLine("Welcome to our slot machine");
             int[,] slotArray = new int[3, 3] { { 0, 0, 0, }, { 0, 0, 0, }, { 0, 0, 0, } };
-
+            int round = 0;
             do
             {
                 for (int i = 0; i < slotArray.GetLength(0); i++)
@@ -18,10 +18,18 @@
                     slotArray[i, 1] = slotArrayRandom.Next(MAXVALUE);
                     slotArray[i, 2] = slotArrayRandom.Next(MAXVALUE);
                 }
-                int round = 0;
+
+
+
+                round++;
+                Console.WriteLine($"Current round:{round}");
+                Console.WriteLine("-------");
+
                 for (int i = 0; i < slotArray.GetLength(0); i++)
+
+
                 {
-                    round++;
+
                     Console.Write($" {slotArray[i, 0]}");
 
                     Console.Write($" {slotArray[i, 1]}");
@@ -29,38 +37,36 @@
                     Console.WriteLine($" {slotArray[i, 2]}");
                 }
 
-                for (int i = 0; i < slotArray.GetLength(0); i++)
+                Console.WriteLine("-------");
+
+                if (slotArray[1, 0] == slotArray[1, 1] && slotArray[1, 1] == slotArray[1, 2])
                 {
-                    if (slotArray[2, 0] == slotArray[2, 1] && slotArray[2, 1] == slotArray[2, 2])
-                    {
-                        Console.WriteLine("You have won on the mid horizontal line!!");
-                    }
+                    Console.WriteLine("You have won on the mid horizontal line!!");
+                }
 
-                    if (slotArray[0, 0] == slotArray[0, 1] && slotArray[0, 1] == slotArray[0, 2])
-                    {
-                        Console.WriteLine("You have won on the top horizontal line!");
-                    }
+                if (slotArray[0, 0] == slotArray[0, 1] && slotArray[0, 1] == slotArray[0, 2])
+                {
+                    Console.WriteLine("You have won on the top horizontal line!");
+                }
 
-                    if (slotArray[2, 0] == slotArray[2, 1] && slotArray[2, 1] == slotArray[2, 2])
-                    {
-                        Console.WriteLine("You have won on the bottom horizontal line!");
-                    }
+                if (slotArray[2, 0] == slotArray[2, 1] && slotArray[2, 1] == slotArray[2, 2])
+                {
+                    Console.WriteLine("You have won on the bottom horizontal line!");
+                }
 
-                    if (slotArray[0, 0] == slotArray[1, 0] && slotArray[1, 0] == slotArray[2, 0])
-                    {
-                        Console.WriteLine("You have won on the first vertical line");
-                    }
+                if (slotArray[0, 0] == slotArray[1, 0] && slotArray[1, 0] == slotArray[2, 0])
+                {
+                    Console.WriteLine("You have won on the first vertical line");
+                }
 
-                    if (slotArray[0, 1] == slotArray[1, 1] && slotArray[1, 1] == slotArray[2, 1])
-                    {
-                        Console.WriteLine("You have won on the second vertical line");
-                    }
+                if (slotArray[0, 1] == slotArray[1, 1] && slotArray[1, 1] == slotArray[2, 1])
+                {
+                    Console.WriteLine("You have won on the second vertical line");
+                }
 
-                    if (slotArray[0, 2] == slotArray[1, 2] && slotArray[1, 2] == slotArray[2, 2])
-                    {
-                        Console.WriteLine("You have won on the the third vertical line");
-                    }
-
+                if (slotArray[0, 2] == slotArray[1, 2] && slotArray[1, 2] == slotArray[2, 2])
+                {
+                    Console.WriteLine("You have won on the the third vertical line");
                 }
 
                 Console.WriteLine("press any key to roll again");
